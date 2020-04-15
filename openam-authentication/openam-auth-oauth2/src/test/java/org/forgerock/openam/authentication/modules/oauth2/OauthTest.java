176c40ffd8eee5c641284648d2552f396a04c2ba
amadmin <ohkubo.yuji4slack@gmail.com>
2020-04-15 04:23:33 -0400

    モック追加中。とりあえず中断

--- openam-authentication/openam-auth-oauth2/src/test/java/org/forgerock/openam/authentication/modules/oauth2/OauthTest.java
+++ openam-authentication/openam-auth-oauth2/src/test/java/org/forgerock/openam/authentication/modules/oauth2/OauthTest.java
@@ -9,6 +9,13 @@ import com.sun.identity.authentication.service.AuthD;
 import com.iplanet.services.naming.WebtopNaming;
 import com.iplanet.am.util.SystemProperties;
 import com.sun.identity.shared.Constants;
+import com.iplanet.services.comm.client.PLLClient;
+import java.util.Vector;
+import java.util.Hashtable;
+import java.util.HashMap;
+import com.iplanet.services.comm.share.RequestSet;
+import com.iplanet.services.naming.share.NamingResponse;
+import com.sun.identity.authentication.share.AuthXMLTags;
 
 import org.junit.Test;
 import org.junit.Before;
@@ -16,32 +23,52 @@ import org.junit.runner.RunWith;
 import org.junit.Assert;
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import org.mockito.ArgumentCaptor;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.modules.junit4.PowerMockRunner;
+import org.mockito.AdditionalAnswers;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({AdminUtils.class,SystemProperties.class})
+@PrepareForTest({AdminUtils.class,SystemProperties.class,PLLClient.class,WebtopNaming.class})
 @PowerMockIgnore("javax.net.ssl.*")
 public class OauthTest{
 
     private WireMockRule mockRule;
 
     @Before
-    public void setup(){
-        PowerMockito.mockStatic(AdminUtils.class);
-        PowerMockito.mockStatic(SystemProperties.class);
-        //AuthD mockAuth = mock(AuthD.class);
-        when(AdminUtils.getAdminPassword()).thenReturn(new byte[10]);
-        when(SystemProperties.get(Constants.AM_NAMING_URL)).thenReturn("/");
-        WebtopNaming.initialize();
+    public void setup() throws Exception{
         mockRule = new WireMockRule(WireMockConfiguration
             .wireMockConfig()
             .dynamicPort()
             .usingFilesUnderDirectory("src/test/java/org/forgerock/openam/authentication/modules/oauth2")
             );
         mockRule.start();
+
+        String httpBase = "http://localhost:"+mockRule.port();
+
+
+        PowerMockito.mockStatic(AdminUtils.class);
+        when(AdminUtils.getAdminPassword()).thenReturn(new byte[10]);
+
+        PowerMockito.mockStatic(SystemProperties.class);
+        when(SystemProperties.get(anyString(),anyString())).then(AdditionalAnswers.returnsSecondArg()); //常に台に引数を有効に
+        when(SystemProperties.get(Constants.AM_NAMING_URL)).thenReturn(httpBase+"/");
+        when(SystemProperties.get("com.sun.identity.agents.app.username")).thenReturn("appUser");   //APP_USERNAME
+        when(SystemProperties.get("com.iplanet.am.service.secret")).thenReturn("appSecret");   //APP_SECRET
+        when(SystemProperties.get("com.iplanet.am.service.password")).thenReturn("appPassword");   //APP_PASSWORD
+
+        PowerMockito.mockStatic(WebtopNaming.class);
+        PowerMockito.doReturn(new Hashtable()).when(WebtopNaming.class,"getNamingTable",any(URL.class));
+        Vector authUrls = new Vector();
+        URL authURL = new URL(httpBase+"/auth");
+        authUrls.add(authURL);
+        when(WebtopNaming.getServiceAllURLs(AuthXMLTags.AUTH_SERVICE)).thenReturn(authUrls);
+
+        PowerMockito.mockStatic(PLLClient.class);
+        when(PLLClient.send(eq(authURL),any(RequestSet.class),any(HashMap.class))).thenReturn(new Vector());
+
     }
 
     @Test
