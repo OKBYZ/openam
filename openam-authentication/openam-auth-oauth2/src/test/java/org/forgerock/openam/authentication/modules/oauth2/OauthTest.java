fa398cbd48da8ab8bb074a8994b57af7be8dc6fa
amadmin <ohkubo.yuji4slack@gmail.com>
2020-04-14 20:52:47 -0400

    wiremockの組み込みはじめ。まだOAuthインスタンスの生成でエラーになる

---
+++ openam-authentication/openam-auth-oauth2/src/test/java/org/forgerock/openam/authentication/modules/oauth2/OauthTest.java
@@ -0,0 +1,54 @@
+package org.forgerock.openam.authentication.modules.oauth2;
+
+import static org.mockito.Mockito.*;
+import java.net.*;
+
+import javax.security.auth.login.LoginException;
+import com.sun.identity.authentication.service.AuthD;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.junit.Assert;
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({AuthD.class})
+@PowerMockIgnore("javax.net.ssl.*")
+public class OauthTest{
+
+    private WireMockRule mockRule;
+
+    @Before
+    public void setup(){
+        PowerMockito.mockStatic(AuthD.class);
+        AuthD mockAuth = mock(AuthD.class);
+        when(AuthD.getAuth()).thenReturn(mockAuth);
+
+        mockRule = new WireMockRule(WireMockConfiguration
+            .wireMockConfig()
+            .dynamicPort()
+            .usingFilesUnderDirectory("src/test/java/org/forgerock/openam/authentication/modules/oauth2")
+            );
+        mockRule.start();
+    }
+
+    @Test
+    public void getContentStreamByGETTest() throws Exception{
+        OAuth oauth = new OAuth();
+        //oauth.getContentStreamByGET("http://localhost:"+mockRule.port()+"/test",null,null);
+        HttpURLConnection con1 = (HttpURLConnection)( new URL("http://localhost:"+mockRule.port()+"/hoge.html").openConnection());
+        Assert.assertEquals(con1.getResponseCode(),400);
+        HttpURLConnection con2 = (HttpURLConnection)( new URL("http://localhost:"+mockRule.port()+"/hage.html").openConnection());
+        Assert.assertEquals(con2.getResponseCode(),500);
+        HttpURLConnection con3 = (HttpURLConnection)( new URL("http://localhost:"+mockRule.port()+"/hoge.html?test1=1&test2=2").openConnection());
+        Assert.assertEquals(con3.getResponseCode(),300);
+        HttpURLConnection con4 = (HttpURLConnection)( new URL("http://localhost:"+mockRule.port()+"/hoge.html?test1=a&test2=2").openConnection());
+        Assert.assertEquals(con4.getResponseCode(),301);
+    }
+}
